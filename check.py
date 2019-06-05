93c3d9fec79393bc37828d2581a39ab2a81147a3
sakakinox <sakakinox@gmail.com>
2019-06-05 17:15:26 +0900

    できた

---
+++ check.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import json
+import toml
+import requests
+
+if (os.path.exists("config.toml")!=1):
+    print (2)
+    sys.exit ("config.tomlがありません。")
+
+config = toml.load(open("config.toml"))
+if (len(sys.argv)<2):
+    print(2)
+    sys.exit ("引数が足りません。")
+
+serverid = sys.argv[1]
+if (serverid == ""):
+    print (2)
+    sys.exit ("serveridが設定されていません。")
+apikey = config["API"]["key"]
+apisecret = config["API"]["secret"]
+url = config["API"]["url"]
+
+r = requests.get(url+serverid, auth=(apikey,apisecret))
+results = json.loads(r.text)
+
+host_url = results["Server"]["Instance"]["Host"]["InfoURL"]
+
+if(host_url !=""):
+    print (1)
+else :
+    print (0)
+
