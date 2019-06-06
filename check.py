c3acad9a93259a40009b23e1269050d496511cbd
sakakinox <sakakinox@gmail.com>
2019-06-06 14:46:47 +0900

    ipを引数にするように変更　単体で動くように変更

--- check.py
+++ check.py
@@ -2,32 +2,45 @@
 import sys
 import os
 import json
-import toml
 import requests
 
-if (os.path.exists("/usr/local/src/sakuracloud-maintenance-checker/config.toml")!=1):
-    print (2)
-    sys.exit ("config.tomlがありません。")
+apikey = ""
+apisecret = ""
+url = ""
 
-config = toml.load(open("/usr/local/src/sakuracloud-maintenance-checker/config.toml"))
+def get_json (url,key,secret):
+    r = requests.get(url, auth=(key,secret))
+    result = json.loads(r.text)
+    return(result)
+
+def get_infourl (json,ip,infourl = ""):
+    num = len(json["Servers"])
+    for i in range(num):
+        instance =json["Servers"][i]
+
+        if (instance["Interfaces"][0]["IPAddress"]==ip):
+            infourl = instance["Instance"]["Host"]["InfoURL"]
+    return(infourl)
+
+
+
 if (len(sys.argv)<2):
     print(2)
     sys.exit ("引数が足りません。")
 
-serverid = sys.argv[1]
-if (serverid == ""):
-    print (2)
-    sys.exit ("serveridが設定されていません。")
-print (serverid)
-sys.exit
+ipaddr = sys.argv[1]
+if (ipaddr == ""):
+    sys.exit ("ipアドレスが設定されていません。")
+#print (serverid)
 
-apikey = config["API"]["key"]
-apisecret = config["API"]["secret"]
-url = config["API"]["url"]
 
-r = requests.get(url+serverid, auth=(apikey,apisecret))
-results = json.loads(r.text)
 
+
+results = get_json(url,apikey,apisecret)
+#print (json.dumps(results["Servers"][28]["Index"],indent=2))
+#print (len(results["Servers"]))
+#print (get_instanceid(results,serverid))
+print (get_infourl(results,ipaddr))
 #host_url = results["Server"]["Instance"]["Host"]["InfoURL"]
 
 #if(host_url !=""):
