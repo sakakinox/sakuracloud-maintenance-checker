f2ef26edf23be90da0267d4d612a2a09b46ab12a
ritsukist <ritsuki.sato@gmail.com>
2022-01-20 13:33:40 +0900

    共用セグメントのIPアドレス以外でもIPを指定できるよう修正

--- check.py
+++ check.py
@@ -3,6 +3,12 @@ import sys
 import json
 import requests
 
+# url = https://secure.sakura.ad.jp/cloud/zone/tk1a/api/cloud/1.1/ (東京第1ゾーン)
+# url = https://secure.sakura.ad.jp/cloud/zone/tk1b/api/cloud/1.1/ (東京第2ゾーン)
+# url = https://secure.sakura.ad.jp/cloud/zone/is1a/api/cloud/1.1/ (石狩第1ゾーン)
+# url = https://secure.sakura.ad.jp/cloud/zone/is1b/api/cloud/1.1/ (石狩第2ゾーン)
+# url = https://secure.sakura.ad.jp/cloud/zone/tk1v/api/cloud/1.1/ (Sandbox)
+
 apikey = ""
 apisecret = ""
 url = ""
@@ -18,24 +24,30 @@ def get_json (url,key,secret):
     result = json.loads(r.text)
     return(result)
 
-
 def get_infourl (json,ip,infourl = ""):
-    num = len(json["Servers"])
-    for i in range(num):
-        instance =json["Servers"][i]
+    for server in json["Servers"]:
+        for interface in server["Interfaces"]:
+            if interface["IPAddress"]==ip or interface["UserIPAddress"]==ip:
+                try:
+                    infourl = server["Instance"]["Host"]["InfoURL"]
+                except TypeError as e:
+                    return "Type Error (server may be powered off)"
+#                infourl ="https://test/"
+                if not infourl:
+                    return "0"
+                else:
+                    return(infourl)
 
-        if (instance["Interfaces"][0]["IPAddress"]==ip):
-            infourl = instance["Instance"]["Host"]["InfoURL"]
-
-
-    return(infourl)
-
+    return "ip address not found"
 
 if (len(sys.argv)<2):
     print(2)
     sys.exit ("引数が足りません。")
 
 ipaddr = sys.argv[1]
+if (ipaddr == ""):
+    sys.exit ("ipアドレスが設定されていません。")
 
-results = get_json(url+"server/",apikey,apisecret)
+results = get_json(url,apikey,apisecret)
+
 print (get_infourl(results,ipaddr))
